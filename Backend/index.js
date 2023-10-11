cfb9751e2ff325523ad85c94dc9768624439a1d7
Muhammad Umar <105128736+zevilzee@users.noreply.github.com>
2023-10-11 18:25:22 +0500

    server setup

---
+++ Backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running");
+});
