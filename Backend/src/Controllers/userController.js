1ab8056b9b45ab89a0f87940bc641dff3608a957
Muhammad Umar <105128736+zevilzee@users.noreply.github.com>
2023-10-22 20:57:31 +0500

    Update userController.js

--- Backend/src/Controllers/userController.js
+++ Backend/src/Controllers/userController.js
@@ -6,57 +6,60 @@ import jwt from "jsonwebtoken";
 // user/sign-up route POST
 export const signUp = async (req, res, next) => {
   console.log(req.body);
- 
+
   //hashing the password
   let hash = await hashSync(req.body.password);
 
   let user = new UserModel({ ...req.body, password: hash });
   await user.save();
 
-   res
-    .status(200)
-    .json({ success: true, message: "user created successfully" });
+  res.status(200).json({ success: true, message: "user created successfully" });
 };
 
 // user/sign-in route GET   sign-in with phone and password
 export const signIn = async (req, res, next) => {
-  
-  try{
-  const user = await UserModel.findOne({
-    email: req.body?.email,
-  });
-  if (!user){
-    res.status(200).json({message:"Incorrect details",status:400})};
-  const passwordMatched = await user.comparePassword(req.body?.password);
-  if (!passwordMatched) {
-    res.status(200).json({message:"Incorrect details",status:400})};
-  
-  const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET);
+  try {
+    const user = await UserModel.findOne({
+      email: req.body?.email,
+    });
+    if (!user) {
+      res.status(200).json({ message: "Incorrect details", status: 400 });
+    }
+    const passwordMatched = await user.comparePassword(req.body?.password);
+    if (!passwordMatched) {
+      res.status(200).json({ message: "Incorrect details", status: 400 });
+    }
 
-   res
-    .set({ "Access-Control-Expose-Headers": "auth_token" })
-    .header("auth_token", token)
-    .json({ success: true, user });
-  }
-  catch(e)
-  {
-    console.log(e)
+    const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET);
+
+    res
+      .set({ "Access-Control-Expose-Headers": "auth_token" })
+      .header("auth_token", token)
+      .json({ success: true, user });
+  } catch (e) {
+    console.log(e);
   }
 };
 
-export const forgotPassword = async (req,res)=>{
+export const forgotPassword = async (req, res) => {
+  console.log(req.body, req.params.id);
   try {
     let hash = await hashSync(req.body.password);
-    const user = await UserModel.findByIdAndUpdate({_id:req.param.id},{password: hash},{new:true});
-    res.send({status:200,messsage: user});
+    const user = await UserModel.findOne({ phoneNumber: req.params.id });
+
+    const newuser = await UserModel.findByIdAndUpdate(
+      { _id: test._id },
+      { password: hash },
+      { new: true }
+    );
+    console.log(user);
+    res.send({ status: 200, messsage: newuser });
+  } catch (e) {
+    res.send({ status: 500, messsage: "Internal server error" });
   }
-  catch(e)
-  {
-    res.send({status:500,messsage: "Internal server error"})
-  }
-}
+};
 
 //user/me ROUTE GET
 export const me = async (req, res, next) => {
-   res.status(200).json({ success: true, user: req?.user });
+  res.status(200).json({ success: true, user: req?.user });
 };
