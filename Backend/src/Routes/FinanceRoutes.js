58f37334fad0647913779177cbf1ee4ba1aa7821
Muhammad Umar <105128736+zevilzee@users.noreply.github.com>
2023-11-17 15:16:42 +0500

    Update FinanceRoutes.js

--- Backend/src/Routes/FinanceRoutes.js
+++ Backend/src/Routes/FinanceRoutes.js
@@ -4,6 +4,7 @@ import {
   createFinance,
   updateFinance,
   deleteFinance,
+  GetFinanceHistory,
 } from "../Controllers/FinanceController.js";
 
 const router = Router();
@@ -11,5 +12,6 @@ router.post("/create-finance", createFinance);
 router.patch("/update-finance/:id", updateFinance);
 router.delete("/delete-finance/:id", deleteFinance);
 router.get("/getFinance/:id", GetFinance);
+router.get("/get-finance-history", GetFinanceHistory);
 
 export default router;
